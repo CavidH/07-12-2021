d655f4bcbf26360acc1106e36906ca5999edca04
Cavid <hcavid386@gmail.com>
2021-12-07 02:40:05 +0400

    2:40

--- Assets/Javascript/Script.js
+++ Assets/Javascript/Script.js
@@ -1,18 +1,19 @@
 basket={}
 let products = document.getElementsByClassName("plus");
 let count = 0;
-console.log(products)
+// console.log(products)
 
 for (const product of products) {
     // console.log(iterator);
     product.addEventListener('click', () => {
-        addToBasket();
+        console.log(product.parentElement.parentElement.parentElement.classList)
+        // addToBasket();
     });
 }
 
-function addToBasket() {
-    count += 1
-    let basket = document.querySelector(".count")
-    console.log(basket)
-basket.innerText = count
-}
+// function addToBasket() {
+//     count += 1
+//     let basket = document.querySelector(".count")
+//     console.log(basket)
+// basket.innerText = count
+// }
