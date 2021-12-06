17bb5ddd64404bf402e5fde38c36f823c8d54b4e
Cavid <hcavid386@gmail.com>
2021-12-07 03:26:22 +0400

    3:26

--- Assets/Javascript/Script.js
+++ Assets/Javascript/Script.js
@@ -1,31 +1,45 @@
-basket={}
+let basket = []
 let products = document.getElementsByClassName("plus");
 let count = 0;
- 
+
+
+ basket.push({name:"testmodel",count:45})
+
 // console.log(products)
 
 for (const product of products) {
     // console.log(iterator);
-    product.addEventListener('click', () => {
-        if (yoxla)
-        console.log(product.parentElement.parentElement.parentElement.classList[0])
-        // addToBasket();
+    product.addEventListener('mouseup', () => {
+        // if (yoxla)
+        // console.log(product.parentElement.parentElement.parentElement.classList[0])
+        checkBasket(product.parentElement.parentElement.parentElement.classList[0])
+        console.log(basket)
+        //    addToBasket();
     });
 }
 
 
-function checkBasket(productName){
-for (let index = 0; index < basket.length; index++) {
-    if (basket[index].name==productName){
-        basket[index].count++;
-    }
-
-    }
+function checkBasket(productName) {
+    // if (basket == null) {
+        for (let index = 0; index < basket.length; index++) {
+            if (basket[index].name == productName) {
+                basket[index].count++;
+            } else {
+                addElementToBasket(productName)
+            }
+        }
+    // } else {
+    //     console.log("leng 0 di")
+    //     addElementToBasket(productName)
+    // }
 }
-function addElementToBasket(productName){
-    basket.add([name:productName,
-        count:0
-    ])
+function addElementToBasket(productName) {
+    console.log("element add ")
+
+    basket.push({
+        name: productName,
+        count: 0
+    });
 }
 
 // function addToBasket() {
