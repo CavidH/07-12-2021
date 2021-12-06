be5311e29c1f6672e068efd6633cb95fe69c1f80
Cavid <hcavid386@gmail.com>
2021-12-07 02:47:59 +0400

    2:47

--- Assets/Javascript/Script.js
+++ Assets/Javascript/Script.js
@@ -1,14 +1,31 @@
 basket={}
 let products = document.getElementsByClassName("plus");
 let count = 0;
+ 
 // console.log(products)
 
 for (const product of products) {
     // console.log(iterator);
     product.addEventListener('click', () => {
+        if (yoxla)
         console.log(product.parentElement.parentElement.parentElement.classList[0])
         // addToBasket();
     });
+}
+
+
+function checkBasket(productName){
+for (let index = 0; index < basket.length; index++) {
+    if (basket[index].name==productName){
+        basket[index].count++;
+    }
+
+    }
+}
+function addElementToBasket(productName){
+    basket.add([name:productName,
+        count:0
+    ])
 }
 
 // function addToBasket() {
