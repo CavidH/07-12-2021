73e81e7b08b7f2e8d19a1fddd55a5b23a66e2ee4
Cavid <hcavid386@gmail.com>
2021-12-07 04:20:57 +0400

    4:20

--- Assets/Javascript/Script.js
+++ Assets/Javascript/Script.js
@@ -1,5 +1,8 @@
 let basket = []
-let products = document.getElementsByClassName("plus");
+let addproducts = document.getElementsByClassName("plus");
+//console.log(addproducts)
+let removeproducts = document.getElementsByClassName("minus");
+//console.log(removeproducts)
 let count = 0;
 
 
@@ -7,18 +10,68 @@ basket.push({ name: "testmodel", count: 45 })
 
 // console.log(products)
 
-for (const product of products) {
-    // console.log(iterator);
-    product.addEventListener('mouseup', () => {
-        // if (yoxla)
-        // console.log(product.parentElement.parentElement.parentElement.classList[0])
-        checkBasket(product.parentElement.parentElement.parentElement.classList[0])
-        console.log(basket)
-        //    addToBasket();
-    });
+addProduct()
+removeProduct()
+
+//add
+function addProduct() {
+    for (const product of addproducts) {
+        // console.log(iterator);
+        product.addEventListener('mouseup', () => {
+            // if (yoxla)
+            // console.log(product.parentElement.parentElement.parentElement.classList[0])
+            checkBasket(product.parentElement.parentElement.parentElement.classList[0])
+            addProductBasketForCount();
+            console.log(basket)
+
+        });
+
+    }
 }
 
 
+//remove
+function removeProduct() {
+    for (const product of removeproducts) {
+        // console.log(iterator);
+        product.addEventListener('mouseup', () => {
+            // if (yoxla)
+            // console.log(product.parentElement.parentElement.parentElement.classList[0])
+            checkBasketForRemove(product.parentElement.parentElement.parentElement.classList[0])
+            // removeProductBasketForCount();
+            console.log(basket)
+
+        });
+
+    }
+}
+
+
+
+function checkBasketForRemove(productName) {
+    for (let index = 0; index < basket.length; index++) {
+        if (basket[index].name == productName) {
+            if (basket[index].count > 0) {
+                basket[index].count--;
+                removeProductBasketForCount();
+            }else{
+                alert("Sebetnizde qeyd olunan mehsul artiq yoxdur")
+            }
+
+            return;
+        }
+
+        // console.log(basket[index].name)
+    }
+    for (let index = 0; index < basket.length; index++) {
+        if (basket[index].name != productName) {
+            alert("Sebetnizde qeyd olunan mehsul yoxdur");
+            return;
+        }
+
+        // console.log(basket[index].name)
+    }
+}
 function checkBasket(productName) {
     for (let index = 0; index < basket.length; index++) {
         if (basket[index].name == productName) {
@@ -37,11 +90,7 @@ function checkBasket(productName) {
 
         // console.log(basket[index].name)
     }
-
-
-
-
-
+}
 
     // let flag = true;
     // for (let index = 0; index < basket.length; index++) {
@@ -60,7 +109,7 @@ function checkBasket(productName) {
     //     console.log("leng 0 di")
     //     addElementToBasket(productName)
     // }
-}
+
 function addElementToBasket(productName) {
     console.log("element add ")
 
@@ -70,9 +119,19 @@ function addElementToBasket(productName) {
     });
 }
 
-// function addToBasket() {
-//     count += 1
-//     let basket = document.querySelector(".count")
-//     console.log(basket)
-// basket.innerText = count
-// }
+function addProductBasketForCount() {
+    count += 1
+    let basket = document.querySelector(".count")
+    console.log(basket)
+    basket.innerText = count
+}
+function removeProductBasketForCount(){
+    if(count>0){
+    count -= 1
+    let basket = document.querySelector(".count")
+    console.log(basket)
+    basket.innerText = count}
+    else{
+        alert("sebet bosdu")
+    }
+}
